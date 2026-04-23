85c48e5a1bbf1eacc82827136916b9496d7e61b4
Angelina Roundfield <aroundfi@umich.edu>
2026-04-22 20:17:27 -0400

    these are the changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-const addNumbers = (first, second) => {
-  return first + second;
-}
-
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
-
+document.querySelector('#button')
+  .addEventListener('click', () => {
+    document.querySelector('#output')
+      .textContent = 'world';
+})
